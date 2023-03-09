59329685cb59dfa83f56a9b6806887b6d967d1de
Hector Vargas <hector.vargas.torrejon@gmail.com>
2023-03-08 21:33:19 -0300

    creacion de los servicios de api getway

--- src/main/java/com/harry/springbootmicroservice3apigateway/service/IUserService.java
+++ src/main/java/com/harry/springbootmicroservice3apigateway/service/IUserService.java
@@ -1,4 +1,16 @@
 package com.harry.springbootmicroservice3apigateway.service;
 
+import com.harry.springbootmicroservice3apigateway.model.Roles;
+import com.harry.springbootmicroservice3apigateway.model.User;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
 public interface IUserService {
+    User saveUser(User user);
+
+    Optional<User> findByUsername(String username);
+
+    @Transactional
+    void changeRoles(Roles newRole, String username);
 }
--- src/main/java/com/harry/springbootmicroservice3apigateway/service/UserServiceImpl.java
+++ src/main/java/com/harry/springbootmicroservice3apigateway/service/UserServiceImpl.java
@@ -1,14 +1,63 @@
 package com.harry.springbootmicroservice3apigateway.service;
 
+import com.harry.springbootmicroservice3apigateway.model.Roles;
+import com.harry.springbootmicroservice3apigateway.model.User;
 import com.harry.springbootmicroservice3apigateway.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements IUserService{
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
+
+    /**
+     * Guardar un User
+     * @param user
+     * @return el usuario guardado
+     */
+    @Override
+    public User saveUser(User user){
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setRole(Roles.USER);
+        user.setFechaCreacion(LocalDateTime.now());
+
+        return userRepository.save(user);
+
+    }
+
+    /**
+     * Buscar un user por username
+     * @param username
+     * @return
+     */
+    @Override
+    public Optional<User> findByUsername(String username){
+        return userRepository.findByUsername(username);
+    }
+
+    /**
+     * Actualiza los roles de un usuario
+     * @param newRole
+     * @param username
+     */
+    @Override
+    @Transactional // se agrega cuando trabajas con query en tu codigo
+    public void changeRoles(Roles newRole, String username){
+        userRepository.updaateUserROle(username, newRole);
+    }
+
+
+
 }
